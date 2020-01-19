bedd7bc3cf2ea7375ccfd5915647ed92926a0ab1
Lamba92 <basti.lamberto@gmail.com>
2020-01-19 22:10:04 +0100

    Well, travis now seems to work. i just need a macbook or hackintosh

--- install_android_sdk.sh
+++ install_android_sdk.sh
@@ -1,8 +1,8 @@
 mkdir -p ~/.android && touch ~/.android/repositories.cfg
-echo "sdkmanager --update"
+echo " - sdkmanager --update"
 sdkmanager --update > /dev/null 2>&1
-echo "sdkmanager --licenses"
+echo " - sdkmanager --licenses"
 yes | sdkmanager --licenses > /dev/null 2>&1
-echo "sdkmanager \"platform-tools\" \"platforms;android-29\" \"build-tools;29.0.2\""
+echo " - sdkmanager \"platform-tools\" \"platforms;android-29\" \"build-tools;29.0.2\""
 sdkmanager "platform-tools" "platforms;android-29" "build-tools;29.0.2" > /dev/null 2>&1
 yes | sdkmanager --licenses > /dev/null 2>&1
